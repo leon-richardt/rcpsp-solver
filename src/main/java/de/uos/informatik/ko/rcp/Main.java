03caab2bf3c3fd8750525fa644db367edeb1367f
Leon Richardt <leon.richardt@gmail.com>
2021-05-04 14:29:16 +0200

    initial commit

---
+++ src/main/java/de/uos/informatik/ko/rcp/Main.java
@@ -0,0 +1,14 @@
+package de.uos.informatik.ko.rcp;
+
+import de.uos.informatik.ko.rcp.Instance;
+import de.uos.informatik.ko.rcp.Io;
+
+import java.nio.file.Paths;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("<TODO>");
+    }
+
+}
