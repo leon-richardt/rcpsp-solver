cd867cc047cb7f2c5ccf82b3e5de7a2621bb8a3b
Benedikte Lechtermann <84265558+blechtermann@users.noreply.github.com>
2021-05-25 13:56:42 +0200

    Fixed Main
    Now the optimal solution from the genetic algorithm is used and is put in GAsol.txt

--- src/main/java/de/uos/informatik/ko/rcp/Main.java
+++ src/main/java/de/uos/informatik/ko/rcp/Main.java
@@ -5,9 +5,8 @@ import de.uos.informatik.ko.rcp.Io;
 import de.uos.informatik.ko.rcp.Utils;
 
 import de.uos.informatik.ko.rcp.generators.EarliestStartScheduleGenerator;
-
-import de.uos.informatik.ko.rcp.generators.PriorityRule.SmallestIndexRule;
-import de.uos.informatik.ko.rcp.generators.SerialScheduleGenerator;
+import de.uos.informatik.ko.rcp.geneticalgorithm.GeneticAlgorithm;
+import de.uos.informatik.ko.rcp.geneticalgorithm.GeneratePop;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -25,8 +24,7 @@ public class Main {
 
         final Instance instance = Io.readInstance(Paths.get(path));
 
-        EarliestStartScheduleGenerator EssGen = new EarliestStartScheduleGenerator(instance);
-
+        var essGen = new EarliestStartScheduleGenerator(instance);
 
         for (int i = 0; i < instance.r(); i++) {
             System.out.printf("res %d has %d available units\n", i, instance.resources[i]);
@@ -34,27 +32,20 @@ public class Main {
 
         //TODO richtigen Random vom Nutzer nutzen
         Random random = new Random();
+
         int[] solution = new int[instance.n()];
 
         System.out.println();
 
-        int[] reihenfolge = new int[instance.n()];
+        solution = GeneticAlgorithm.geneticAlgorithm(instance, random);
 
-        // TODO Deep Copy nötig? Eigentlich nicht, oder?
-        reihenfolge = GA.geneticAlgorithm(instance, random);
+        // solution = essGen.generateSchedule(reihenfolge);
 
-        for (int i = 0; i < reihenfolge.length; i++) {
-            System.out.println(reihenfolge[i]);
-        }
+        System.out.println("Makespan: " + solution[solution.length-1]);
 
-        solution = EssGen.generateSchedule(reihenfolge);
-
-        System.out.println("Makespan" + solution[solution.length-1]);
-
-        Io.writeSolution(solution, Paths.get("sol2.txt"));
+        Io.writeSolution(solution, Paths.get("solGA.txt"));
 
         System.out.println("ok");
-
     }
 
 }
